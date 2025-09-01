1e20e3aeeb3f25db35e59aac73e1e2f46ebbd718
zikes18 <josias_zklima@hotmail.com>
2025-09-01 11:42:01 -0300

    pasta duplicada

--- edukacode/src/main/java/br/com/edukacode/api/DadosCadastroLead.java
+++
@@ -1,3 +0,0 @@
-package br.com.edukacode.api;
-
-public record DadosCadastroLead(String nome, String email, String telefone) {}
--- edukacode/src/main/java/br/com/edukacode/api/LeadRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.edukacode.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeadRepository extends JpaRepository <Lead, Long>{
-
-}

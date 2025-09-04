d2035e85e370bac800b7570e98358cd303edb8b7
zikes18 <josias_zklima@hotmail.com>
2025-09-04 11:54:15 -0300

    me´todo delete no Lead

--- src/main/java/br/com/edukacode/api/controller/LeadController.java
+++ src/main/java/br/com/edukacode/api/controller/LeadController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,9 +50,10 @@ public class LeadController {
         var lead = repository.getReferenceById(dados.id());
         lead.atualizarInformacoes(dados);
     }
-    @DeleteMapping
-    public void excluirLead() {
+    @DeleteMapping("/{id}")
+    public void excluirLead(@PathVariable Long id) {
         // Implementação do método para excluir um lead
+        repository.deleteById(id);
     }
 
 }
